cd42b7464615297a631598db2d6edb03fc9674cb
umaima <105052639+umaima222@users.noreply.github.com>
2025-12-23 01:58:23 +0500

    changes

--- inc/headlinks.php
+++ inc/headlinks.php
@@ -9,7 +9,8 @@
 <!-- <link rel="icon" href="/favicon.ico"> -->
 <!-- <link rel="icon" href="https://corevisionsystems.com/img/favicon.ico" type="image/x-icon"> -->
 <!-- <link rel="shortcut icon" href="https://corevisionsystems.com/img/favicon.ico" type="image/x-icon"> -->
-<link rel="icon" href="https://corevisionsystems.com/img/favicon.ico" type="image/vnd.microsoft.icon" sizes="any">
+<!-- <link rel="icon" href="https://corevisionsystems.com/img/favicon.ico" type="image/vnd.microsoft.icon" sizes="any"> -->
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
 
 
 
