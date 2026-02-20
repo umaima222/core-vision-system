837f903a54c2ecd378a8466df4ef93dc582df136
umaima <105052639+umaima222@users.noreply.github.com>
2026-02-20 22:24:04 +0500

    changes

--- inc/headlinks.php
+++ inc/headlinks.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Site Title -->
-<title>Corevision System </title>
+<title>Corevision Systems </title>
 <!-- Site Title -->
 
 <!-- Favicon -->
